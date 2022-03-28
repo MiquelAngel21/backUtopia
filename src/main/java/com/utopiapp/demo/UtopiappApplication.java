c51c1ef23c5890ebfd66cdd01d7bda81838f1b76
Miquel Àngel Cabot Prats <macp2001fcb@gmail.com>
2022-03-28 18:35:36 +0200

    Proyecto inicializado

---
+++ src/main/java/com/utopiapp/demo/UtopiappApplication.java
@@ -0,0 +1,13 @@
+package com.utopiapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtopiappApplication.class, args);
+	}
+
+}
